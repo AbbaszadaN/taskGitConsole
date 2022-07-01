2699282c4ba2f79da0f535382325846aa973e6e2
nezrinabbaszade2@gmail.com <nezrinabbaszade2@gmail.com>
2022-07-01 23:34:47 +0400

    21

--- taskGitConsole/Program.cs
+++ taskGitConsole/Program.cs
@@ -6,7 +6,7 @@ namespace taskGitConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }
